44f40340b09b936ee2292f65b590f352cdeab51d
Julen Cruz <julencruzg@gmail.com>
2026-03-30 13:10:44 +0200

    PORT/create-repositories-for-models-and-main-methods (#3)
    * feat: add TaskRepository interface
    
    * feat: add UserRepository interface
    
    * feat: add HouseRepository interface
    
    * feat: add CardRepository interface
    
    * style: rename saveUser method to save to standardize

---
+++ src/main/kotlin/com/houseclash/backend/domain/port/HouseRepository.kt
@@ -0,0 +1,10 @@
+package com.houseclash.backend.domain.port
+
+import com.houseclash.backend.domain.model.House
+
+interface HouseRepository {
+
+    fun save(house: House) : House
+    fun findById(id: Long): House?
+    fun delete(house: House)
+}
