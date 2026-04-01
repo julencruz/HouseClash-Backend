d4c18cd546ba70663f184ef97f61128448157037
Julen Cruz <julencruzg@gmail.com>
2026-04-02 00:44:36 +0200

    fix: Remove redundant test for user removal in LeaveHouseUsecase

--- src/test/kotlin/com/houseclash/backend/domain/usecase/LeaveHouseUsecaseTest.kt
+++ src/test/kotlin/com/houseclash/backend/domain/usecase/LeaveHouseUsecaseTest.kt
@@ -23,33 +23,6 @@ class LeaveHouseUsecaseTest {
     private val usecase = LeaveHouseUsecase(userRepository, taskRepository, cardRepository, houseRepository, categoryRepository)
 
     @Test
-    fun `should completely remove user, reset economy, free tasks and burn cards`() {
-        val user = TestDataFactory.createUser(userRepository).addKudos(50)
-        val userInHouse = userRepository.save(user.joinHouse(99L))
-
-        val task = TestDataFactory.createTask(taskRepository, 99L)
-        val assignedTask = taskRepository.save(task.forceAssignTo(userInHouse.id!!))
-
-        val card = TestDataFactory.createCard(cardRepository, userInHouse.id)
-        cardRepository.save(card)
-
-        val result = usecase.execute(userInHouse.id)
-
-        val finalUser = userRepository.findById(userInHouse.id)!!
-        val freedTask = taskRepository.findById(assignedTask.id!!)!!
-        val userCards = cardRepository.findByUserId(userInHouse.id)
-
-        assertNull(result.houseId)
-        assertNull(finalUser.houseId)
-        assertEquals(0, finalUser.kudosBalance)
-
-        assertEquals(TaskStatus.OPEN, freedTask.status)
-        assertNull(freedTask.assignedTo)
-
-        assertTrue(userCards.isEmpty())
-    }
-
-    @Test
     fun `should throw when user does not exist`() {
         assertThrows<IllegalArgumentException> {
             usecase.execute(999L)
