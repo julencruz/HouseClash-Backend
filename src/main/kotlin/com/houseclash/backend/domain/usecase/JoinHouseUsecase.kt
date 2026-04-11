b92cf438a16a8c5da704bcfae48c6a179831eb35
Julen Cruz <julencruzg@gmail.com>
2026-04-11 21:20:59 +0200

    feat: Add welcome bonus kudos for first-time house joiners

--- src/main/kotlin/com/houseclash/backend/domain/usecase/JoinHouseUsecase.kt
+++ src/main/kotlin/com/houseclash/backend/domain/usecase/JoinHouseUsecase.kt
@@ -4,11 +4,15 @@ import com.houseclash.backend.domain.model.User
 import com.houseclash.backend.domain.port.HouseRepository
 import com.houseclash.backend.domain.port.UserRepository
 
-class JoinHouseUsecase (
-    private val userRepository : UserRepository,
+class JoinHouseUsecase(
+    private val userRepository: UserRepository,
     private val houseRepository: HouseRepository,
 ) {
-    fun execute(userId: Long, inviteCode: String) : User {
+    companion object {
+        const val WELCOME_BONUS_KUDOS = 50
+    }
+
+    fun execute(userId: Long, inviteCode: String): User {
         val user = userRepository.findById(userId)
         require(user != null) { "User does not exist." }
         require(user.houseId == null) { "User already belongs to a house" }
@@ -16,7 +20,13 @@ class JoinHouseUsecase (
         val house = houseRepository.findByInviteCode(inviteCode)
         require(house != null) { "House not found for invite code: $inviteCode" }
 
-        val updatedUser = user.joinHouse(house.id!!)
+        val isFirstVisit = !user.hasVisitedHouse(house.id!!)
+        var updatedUser = user.joinHouse(house.id)
+
+        if (isFirstVisit) {
+            updatedUser = updatedUser.addKudos(WELCOME_BONUS_KUDOS)
+        }
+
         return userRepository.save(updatedUser)
     }
 }
